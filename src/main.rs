ebd4939c7d40c4f8ff4d380b2be8ef4a16e5a963
Daniel (dmilith) Dettlaff <daniel@centra.com>
2020-08-10 17:15:07 +0200

    new: Don't give attacker any hints. Respond nothing on 404,500 http error codes.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@
         trivial_casts,
         trivial_numeric_casts,
         unused_import_braces,
-        unused_qualifications)]
+        // unused_qualifications,
+        )]
 // #![allow(unused_imports, dead_code)]
 
 
@@ -17,6 +18,7 @@ extern crate rocket;
 
 use crate::config::config;
 use crate::config::validate_config;
+use rocket::request::Request;
 use rocket::{ignite, Rocket};
 
 
@@ -38,10 +40,23 @@ const SERVER_PUBLIC_KEY: &str = "/Services/Wireguard-tools/pub.key";
 const SERVER_PRIVATE_KEY: &str = "/Services/Wireguard-tools/private.key";
 
 
+#[catch(500)]
+fn internal_error() -> &'static str {
+    ""
+}
+
+
+#[catch(404)]
+fn not_found(_req: &Request) -> String {
+    "".to_string()
+}
+
+
 #[launch]
 fn start() -> Rocket {
     // TODO: add validation if os is local vs server
     // TODO: add validation if user is root
+    // #[cfg(os_target = "freebsd")]
     validate_config(&config());
     ignite()
         .mount(
@@ -52,4 +67,5 @@ fn start() -> Rocket {
             &format!("/{}/wireguard/workstation/", config().uuid),
             routes![workstations::new],
         )
+        .register(catchers![internal_error, not_found])
 }
