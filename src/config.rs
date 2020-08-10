78da432733a441a00439b465851cd9f09d658c42
Daniel (dmilith) Dettlaff <daniel@centra.com>
2020-08-10 11:56:31 +0200

    new: Implemented simple Config instead of hardcoding values in code. Defined configuration validation on launch.

---
+++ src/config.rs
@@ -0,0 +1,35 @@
+use serde_derive::Deserialize;
+
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub main_net: String,
+    pub main_net_mask: String,
+    pub server_port: u16,
+    pub server_public_ip: String,
+}
+
+
+pub fn config() -> Config {
+    use std::fs::read_to_string;
+    match toml::from_str(&read_to_string("config/config.toml").unwrap_or_default()) {
+        Ok(config) => config,
+        Err(error) => panic!("Failed to read config: {}", error),
+    }
+}
+
+
+pub fn validate_config(config: &Config) {
+    if config.server_public_ip.is_empty() {
+        panic!("Config: server_public_ip endpoint can't be empty!")
+    }
+    if config.main_net.is_empty() {
+        panic!("Config: main_net can't be empty!")
+    }
+    if config.main_net_mask.is_empty() {
+        panic!("Config: main_net_mask can't be empty!")
+    }
+    if config.server_port == 0 {
+        panic!("Config: server_port can't be 0!")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,12 @@
 #[macro_use]
 extern crate rocket;
 
+use crate::config::config;
+use crate::config::validate_config;
 use rocket::{ignite, Rocket};
 
 
+mod config;
 mod instances;
 mod templates;
 mod utils;
@@ -27,23 +30,13 @@ mod workstations;
 mod tests;
 
 
-const SERVER_IP: &str = "[[ your-external-ip-address ]]";
-const SERVER_PORT: &str = "61194";
 const SERVER_PUBLIC_KEY: &str = "/Services/Wireguard-tools/pub.key";
 const SERVER_PRIVATE_KEY: &str = "/Services/Wireguard-tools/private.key";
-
-const MAIN_NET: &str = "100.64";
-// const MAIN_NET6: &str = "fde4:82c4:4eb:dd8d::";
-const WORKSTATIONS_SUBNET: &str = "1";
-
-const SERVER_ROUTER_IP: &str = "100.64.1.1";
-// const SERVER_ROUTER_IP6: &str = "fde4:82c4:4eb:dd8d::1:1";
-const MAIN_MASK: &str = "/10";
-// const MAIN_MASK6: &str = "/64";
 
 
 #[launch]
 fn start() -> Rocket {
+    validate_config(&config());
     ignite()
         .mount(
             "/796A425F-DD50-4A61-B535-92920EA54818/wireguard/new/instance/",
