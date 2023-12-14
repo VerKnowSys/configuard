8caa3954f20421c00b4046a98d0bc5264dbee929
Daniel (dmilith) Dettlaff <dmilith@me.com>
2023-12-14 14:08:41 +0100

    new: Dropped support for instances

--- src/templates.rs
+++ src/templates.rs
@@ -15,18 +15,6 @@ pub struct WireguardWorkstationTemplate<'a> {
 
 
 #[derive(Template, Debug)]
-#[template(path = "instance.conf", escape = "none")]
-pub struct WireguardInstanceTemplate<'a> {
-    pub user_name: &'a str,
-    pub user_private_key: &'a str,
-    pub user_nets: &'a str,
-    pub server_router_ip: &'a str,
-    pub server_public_key: &'a str,
-    pub default_server_endpoint: &'a str,
-}
-
-
-#[derive(Template, Debug)]
 #[template(path = "server.conf", escape = "none")]
 pub struct WireguardServerConfigurationTemplate<'a> {
     pub server_port: &'a str,
